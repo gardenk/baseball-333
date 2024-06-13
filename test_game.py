a9587d69c3164a1faff3c012b64433432892688c
gardenk <jwnnekim@gmail.com>
2024-06-13 14:47:21 +0900

    [refactor] p.58

--- test_game.py
+++ test_game.py
@@ -9,12 +9,11 @@ class TestGame(TestCase):
         super().setUp()
         self.game = Game()
 
-    def test_exception_invalid_input(self):
-        self.assert_invalid_argument(None)
-        self.assert_invalid_argument("12")
-        self.assert_invalid_argument("1234")
-        self.assert_invalid_argument("12s")
-        self.assert_invalid_argument("121")
+    def assert_matched_number(self, result, solved, strikes, balls):
+        self.assertIsNotNone(result)
+        self.assertEqual(solved, result.get_solved())
+        self.assertEqual(strikes, result.get_strikes())
+        self.assertEqual(balls, result.get_ball())
 
     def assert_invalid_argument(self, guessNum):
         try:
@@ -23,20 +22,20 @@ class TestGame(TestCase):
         except TypeError:
             pass
 
-    def test_solve_result_if_matched_number(self):
-        self.game.question = "123"
-        result: GameResult = self.game.guess("123")
+    def test_exception_invalid_input(self):
+        self.assert_invalid_argument(None)
+        self.assert_invalid_argument("12")
+        self.assert_invalid_argument("1234")
+        self.assert_invalid_argument("12s")
+        self.assert_invalid_argument("121")
 
-        self.assertIsNotNone(result)
-        self.assertTrue(result.get_solved())
-        self.assertEqual(3, result.get_strikes())
-        self.assertEqual(0, result.get_ball())
+    def generate_question(self, question_num):
+        self.game.question = "123"
+
+    def test_solve_result_if_matched_number(self):
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("123"), True, 3, 0)
 
     def test_solve_result_if_unmatched_number(self):
-        self.game.question = "123"
-        result: GameResult = self.game.guess("456")
-
-        self.assertIsNotNone(result)
-        self.assertFalse(result.get_solved())
-        self.assertEqual(0, result.get_strikes())
-        self.assertEqual(0, result.get_ball())
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("456"), False, 0, 0)
