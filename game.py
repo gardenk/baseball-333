9e371ab573a28701306d021d940b37434be936e6
gardenk <jwnnekim@gmail.com>
2024-06-13 14:04:33 +0900

    [refactor] p.43

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 class Game:
     def guess(self, num):
+        self.assert_invalid_value(num)
+
+    def assert_invalid_value(self, num):
         if num is None or len(num) != 3:
             raise TypeError
-
         for N in num:
             if not ord('0') <= ord(N) <= ord('9'):
                 raise TypeError
-
         if len(set(list(num))) < 3:
             raise TypeError
